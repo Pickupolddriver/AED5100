ebe9b64dfec51dba463a57bd70f15e0c8ee4e7a7
JianxiLi <li.jianx@husky.neu.edu>
2018-01-18 18:45:22 -0500

    submit the Assignment1

---
+++ Assignment1_2018/src/Human/Person.java
@@ -0,0 +1,134 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Human;
+
+import java.util.Date;
+
+/**
+ *
+ * @author lijianxi
+ */
+ 
+public class Person {
+    private String name;
+    private String geographicdata;
+    private String dateOfBirth;
+    private String telephoneNum;
+    private String faxNumber;
+    private String emailAdd;
+    private int SSN;
+    private int medicalNumber;
+    private double bankAccNum;
+    private String linkedIn;
+    private int HPBN;
+    private String Filename;
+
+    public String getFilename() {
+        return Filename;
+    }
+
+    public void setFilename(String Filename) {
+        this.Filename = Filename;
+    }
+
+   
+    
+    
+    public int getHPBN() {
+        return HPBN;
+    }
+
+    public void setHPBN(int HPBN) {
+        this.HPBN = HPBN;
+    }
+    
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGeographicdata() {
+        return geographicdata;
+    }
+
+    public void setGeographicdata(String geographicdata) {
+        this.geographicdata = geographicdata;
+    }
+
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public double getBankAccNum() {
+        return bankAccNum;
+    }
+
+    public void setBankAccNum(double bankAccNum) {
+        this.bankAccNum = bankAccNum;
+    }
+
+   
+
+    public String getTelephoneNum() {
+        return telephoneNum;
+    }
+
+    public void setTelephoneNum(String telephoneNum) {
+        this.telephoneNum = telephoneNum;
+    }
+
+    public String getFaxNumber() {
+        return faxNumber;
+    }
+
+    public void setFaxNumber(String faxNumber) {
+        this.faxNumber = faxNumber;
+    }
+
+    public String getEmailAdd() {
+        return emailAdd;
+    }
+
+    public void setEmailAdd(String emailAdd) {
+        this.emailAdd = emailAdd;
+    }
+
+    public int getSSN() {
+        return SSN;
+    }
+
+    public void setSSN(int SSN) {
+        this.SSN = SSN;
+    }
+
+    public int getMedicalNumber() {
+        return medicalNumber;
+    }
+
+    public void setMedicalNumber(int medicalNumber) {
+        this.medicalNumber = medicalNumber;
+    }
+
+    public String getLinkedIn() {
+        return linkedIn;
+    }
+
+    public void setLinkedIn(String linkedIn) {
+        this.linkedIn = linkedIn;
+    }
+    
+    
+     
+    
+}
