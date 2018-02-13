ac06bb1bbef2f5f4d219c7062ceb0c1d632b5416
JianxiLi <li.jianx@husky.neu.edu>
2018-02-13 02:19:21 -0500

    2nd commit of HW3

---
+++ Assignment_3_Final_/src/Business/Airplane.java
@@ -0,0 +1,92 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business;
+
+/**
+ *
+ * @author lijianxi
+ */
+public class Airplane {
+    private String airliner;
+    private String manufacture;
+    private String model;
+    private String serialNumber;
+    private int seatnumber=180;
+    private int row=6;
+    private int col=30;
+
+     public Airplane(){
+    }
+    
+    public Airplane(String manufacture,String model,String airliner){
+        this.manufacture=manufacture;
+        this.model=model;
+        this.airliner=airliner;
+    }
+
+    public String getAirliner() {
+        return airliner;
+    }
+
+    public void setAirliner(String airliner) {
+        this.airliner = airliner;
+    }
+    
+    
+    
+    public int getRow() {
+        return row;
+    }
+
+    public void setRow(int row) {
+        this.row = row;
+    }
+
+    public int getCol() {
+        return col;
+    }
+
+    public void setCol(int col) {
+        this.col = col;
+    }
+    
+   
+    
+    public String getManufacture() {
+        return manufacture;
+    }
+
+    public void setManufacture(String manufacture) {
+        this.manufacture = manufacture;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getSeatnumber() {
+        return seatnumber;
+    }
+
+    public void setSeatnumber(int seatnumber) {
+        this.seatnumber = seatnumber;
+    }
+
+    public String getSerialNumber() {
+        return serialNumber;
+    }
+
+    public void setSerialNumber(String serialNumber) {
+        this.serialNumber = serialNumber;
+    }
+    
+    
+    
+}
